cdd3a58ad36dcfda4ea0834e0d47e822e56a538f
Leah Njuguna <munjiruai@gmail.com>
2024-08-27 01:19:26 +0300

    Securely passed the API key from your environment to the Flask app

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
@@ -8,20 +9,23 @@ import bs4 as bs
 import urllib.request
 import pickle
 import requests
+from dotenv import load_dotenv
 
-# load the nlp model and tfidf vectorizer from disk
+load_dotenv()
+TMDB_API_KEY = os.getenv('TMDB_API_KEY')
+# Load the NLP model and TFIDF vectorizer from disk
 filename = './models/nlp_model.pkl'
 clf = pickle.load(open(filename, 'rb'))
-vectorizer = pickle.load(open('./models/tranform.pkl','rb'))
+vectorizer = pickle.load(open('./models/tranform.pkl', 'rb'))
 
 def create_similarity():
     data = pd.read_csv('./data/main_data.csv')
-    # creating a count matrix
+    # Creating a count matrix
     cv = CountVectorizer()
     count_matrix = cv.fit_transform(data['comb'])
-    # creating a similarity score matrix
+    # Creating a similarity score matrix
     similarity = cosine_similarity(count_matrix)
-    return data,similarity
+    return data, similarity
 
 def rcmd(m):
     m = m.lower()
@@ -33,49 +37,44 @@ def rcmd(m):
     if m not in data['movie_title'].unique():
         return('Sorry! The movie you requested is not in our database. Please check the spelling or try with some other movies')
     else:
-        i = data.loc[data['movie_title']==m].index[0]
+        i = data.loc[data['movie_title'] == m].index[0]
         lst = list(enumerate(similarity[i]))
-        lst = sorted(lst, key = lambda x:x[1] ,reverse=True)
-        lst = lst[1:11] # excluding first item since it is the requested movie itself
-        l = []
-        for i in range(len(lst)):
-            a = lst[i][0]
-            l.append(data['movie_title'][a])
+        lst = sorted(lst, key=lambda x: x[1], reverse=True)
+        lst = lst[1:11]  # excluding first item since it is the requested movie itself
+        l = [data['movie_title'][a] for a, _ in lst]
         return l
-    
-# converting list of string to list (eg. "["abc","def"]" to ["abc","def"])
+
 def convert_to_list(my_list):
     my_list = my_list.split('","')
-    my_list[0] = my_list[0].replace('["','')
-    my_list[-1] = my_list[-1].replace('"]','')
+    my_list[0] = my_list[0].replace('["', '')
+    my_list[-1] = my_list[-1].replace('"]', '')
     return my_list
 
 def get_suggestions():
     data = pd.read_csv('./data/main_data.csv')
     return list(data['movie_title'].str.capitalize())
 
-#app = Flask(__name__)
 app = Flask(__name__, static_folder='static')
 
 @app.route("/")
 @app.route("/home")
 def home():
     suggestions = get_suggestions()
-    return render_template('home.html',suggestions=suggestions)
+    return render_template('home.html', suggestions=suggestions, api_key=TMDB_API_KEY)
 
-@app.route("/similarity",methods=["POST"])
+@app.route("/similarity", methods=["POST"])
 def similarity():
     movie = request.form['name']
     rc = rcmd(movie)
-    if type(rc)==type('string'):
+    if isinstance(rc, str):
         return rc
     else:
-        m_str="---".join(rc)
+        m_str = "---".join(rc)
         return m_str
 
-@app.route("/recommend",methods=["POST"])
+@app.route("/recommend", methods=["POST"])
 def recommend():
-    # getting data from AJAX request
+    # Get data from AJAX request
     title = request.form['title']
     cast_ids = request.form['cast_ids']
     cast_names = request.form['cast_names']
@@ -96,10 +95,10 @@ def recommend():
     rec_movies = request.form['rec_movies']
     rec_posters = request.form['rec_posters']
 
-    # get movie suggestions for auto complete
+    # Get movie suggestions for autocomplete
     suggestions = get_suggestions()
 
-    # call the convert_to_list function for every string that needs to be converted to list
+    # Convert necessary strings to lists
     rec_movies = convert_to_list(rec_movies)
     rec_posters = convert_to_list(rec_posters)
     cast_names = convert_to_list(cast_names)
@@ -108,47 +107,30 @@ def recommend():
     cast_bdays = convert_to_list(cast_bdays)
     cast_bios = convert_to_list(cast_bios)
     cast_places = convert_to_list(cast_places)
+    cast_ids = [x.replace("[", "").replace("]", "") for x in cast_ids.split(",")]
+
+    # Clean cast bios
+    cast_bios = [bio.replace(r'\n', '\n').replace(r'\"', '\"') for bio in cast_bios]
+
+    # Combine multiple lists as a dictionary for rendering in HTML
+    movie_cards = dict(zip(rec_posters, rec_movies))
+    casts = {cast_names[i]: [cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
+    cast_details = {cast_names[i]: [cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
+
+    # Web scraping to get user reviews from IMDB site
+    sauce = urllib.request.urlopen(f'https://www.imdb.com/title/{imdb_id}/reviews?ref_=tt_ov_rt').read()
+    soup = bs.BeautifulSoup(sauce, 'lxml')
+    soup_result = soup.find_all("div", {"class": "text show-more__control"})
+
+    reviews_list = [reviews.string for reviews in soup_result if reviews.string]
+    reviews_status = ['Good' if clf.predict(vectorizer.transform([review])) else 'Bad' for review in reviews_list]
+    movie_reviews = dict(zip(reviews_list, reviews_status))
+
+    api_key = TMDB_API_KEY
     
-    # convert string to list (eg. "[1,2,3]" to [1,2,3])
-    cast_ids = cast_ids.split(',')
-    cast_ids[0] = cast_ids[0].replace("[","")
-    cast_ids[-1] = cast_ids[-1].replace("]","")
-    
-    # rendering the string to python string
-    for i in range(len(cast_bios)):
-        cast_bios[i] = cast_bios[i].replace(r'\n', '\n').replace(r'\"','\"')
-    
-    # combining multiple lists as a dictionary which can be passed to the html file so that it can be processed easily and the order of information will be preserved
-    movie_cards = {rec_posters[i]: rec_movies[i] for i in range(len(rec_posters))}
-    
-    casts = {cast_names[i]:[cast_ids[i], cast_chars[i], cast_profiles[i]] for i in range(len(cast_profiles))}
-
-    cast_details = {cast_names[i]:[cast_ids[i], cast_profiles[i], cast_bdays[i], cast_places[i], cast_bios[i]] for i in range(len(cast_places))}
-
-    # web scraping to get user reviews from IMDB site
-    sauce = urllib.request.urlopen('https://www.imdb.com/title/{}/reviews?ref_=tt_ov_rt'.format(imdb_id)).read()
-    soup = bs.BeautifulSoup(sauce,'lxml')
-    soup_result = soup.find_all("div",{"class":"text show-more__control"})
-
-    reviews_list = [] # list of reviews
-    reviews_status = [] # list of comments (good or bad)
-    for reviews in soup_result:
-        if reviews.string:
-            reviews_list.append(reviews.string)
-            # passing the review to our model
-            movie_review_list = np.array([reviews.string])
-            movie_vector = vectorizer.transform(movie_review_list)
-            pred = clf.predict(movie_vector)
-            reviews_status.append('Good' if pred else 'Bad')
-
-    # combining reviews and comments into a dictionary
-    movie_reviews = {reviews_list[i]: reviews_status[i] for i in range(len(reviews_list))}     
-
-    # passing all the data to the html file
-    return render_template('recommend.html',title=title,poster=poster,overview=overview,vote_average=vote_average,
-        vote_count=vote_count,release_date=release_date,runtime=runtime,status=status,genres=genres,
-        movie_cards=movie_cards,reviews=movie_reviews,casts=casts,cast_details=cast_details)
+    return render_template('recommend.html', title=title, poster=poster, overview=overview, vote_average=vote_average,
+    vote_count=vote_count, release_date=release_date, runtime=runtime, status=status, genres=genres,
+    movie_cards=movie_cards, reviews=movie_reviews, casts=casts, cast_details=cast_details, api_key=api_key)
 
 if __name__ == '__main__':
-    
     app.run(debug=True)
